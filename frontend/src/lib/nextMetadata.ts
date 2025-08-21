4eb531dbc571bd62e33a9a9263d18035295a33b5
oarthurfc <arthuryouface@gmail.com>
2025-08-21 20:22:23 -0300

    feat: add static SEO Metadata file

---
+++ frontend/src/lib/nextMetadata.ts
@@ -0,0 +1,24 @@
+const seoData = {
+  title:
+    'LaunchPad - Your content delivery partner for large scale applications',
+  description: `A platform integrating Aceternity with Strapi for seamless content management.`,
+  image: 'https://ui.aceternity.com/banner.png',
+  openGraph: {
+    type: 'website',
+    title: `LaunchPad - Your content delivery partner for large scale applications`,
+    url: 'https://ui.aceternity.com', // TODO: Fix this
+    description: `A platform integrating Aceternity with Strapi for seamless content management.`,
+    locale: 'en_EN',
+    keywords: 'content, delivery, web, hosting',
+    images: [
+      {
+        width: 1200,
+        height: 630,
+        url: `https://ui.aceternity.com/banner.png`, // TODO: Fix this
+      },
+    ],
+    site_name: 'launchpad',
+  }
+};
+
+export default seoData;
