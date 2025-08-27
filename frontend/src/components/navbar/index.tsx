5ca7a041fcbdeb9c53b70af5215e85d478da930c
oarthurfc <arthuryouface@gmail.com>
2025-08-27 00:07:08 -0300

    refactor: melhorar a formatação e a organização do componente Navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -2,47 +2,40 @@ import { strapiImage } from "@/lib/strapi/strapiImage";
 import Image from "next/image";
 
 type Props = {
-    right_navbar_items: {
-    id: string, 
+  right_navbar_items: {
+    id: string;
     URL: string;
     text: string;
     target?: string;
   }[];
   logo: {
-    url: string,
-  }
+    url: string;
+  };
 };
 
 export function Navbar({ data }: { data: Props | null }) {
-    if (!data) return null;
-    
-    console.log("Data recebida no NavBar:" , data)
-    return(
-        <div className="flex flex-row items-center justify-center w-full bg-red-700 h-16 border-b-2 border-amber-300">
+  if (!data) return null;
 
-            <div className="flex flex-row justify-between w-full max-w-[1320px]">
-                {data?.logo && (
-                    <Image 
-                        src={strapiImage(data.logo.url)} 
-                        width={90} 
-                        height={30} 
-                        alt="Logo Gol a Gol"
-                    />                
-                )}
+  console.log("Data recebida no NavBar:", data);
+  return (
+    <div className="flex h-16 w-full flex-row items-center justify-center border-b-2 border-amber-300 bg-black">
+      <div className="flex w-full max-w-[1320px] flex-row justify-between">
+        {data?.logo && (
+          <Image src={strapiImage(data.logo.url)} width={90} height={30} alt="Logo Gol a Gol" />
+        )}
 
-                
-                {data?.right_navbar_items && data.right_navbar_items.length > 0 && (
-                    <div className="flex flex-row">
-                        <h1>Data recebida</h1>
-                        {/* Renderize seus itens de navegação aqui */}
-                        <ul>
-                            {data.right_navbar_items.map((item) => (
-                                <li key={item.id}>{item.text}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+        {data?.right_navbar_items && data.right_navbar_items.length > 0 && (
+          <nav className="flex flex-row">
+            <ul className="flex flex-row gap-12">
+              {data.right_navbar_items.map((item) => (
+                <li key={item.id} className="font-semibold">
+                  {item.text.toUpperCase()}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
+      </div>
+    </div>
+  );
 }
