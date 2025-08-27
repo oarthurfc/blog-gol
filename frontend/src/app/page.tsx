d35c96efd18b7751679530ebdfae6cb8590c1291
oarthurfc <arthuryouface@gmail.com>
2025-08-27 00:04:05 -0300

    refactor: melhorar a formatação e organização do código no componente Home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,54 +5,56 @@ import { strapiImage } from "@/lib/strapi/strapiImage";
 import Image from "next/image";
 
 export async function generateMetadata() {
-  const homepage = await fetchContentType('home', {
-    populate: {
-      seo: { populate: '*' }
-    }
-  }, true);
+  const homepage = await fetchContentType(
+    "home",
+    {
+      populate: {
+        seo: { populate: "*" },
+      },
+    },
+    true,
+  );
   return generateMetadataObject(homepage?.seo);
 }
 
 export default async function Home() {
-  const homepage = await fetchContentType('home', {
-    populate: {
-      top_banner: { populate: '*' },
-      main_article: { populate: '*' },
-      middle_banner: { populate: '*' },
-      bet_main_article: { populate: '*' }
-    }
-  }, true);
+  const homepage = await fetchContentType(
+    "home",
+    {
+      populate: {
+        top_banner: { populate: "*" },
+        main_article: { populate: "*" },
+        middle_banner: { populate: "*" },
+        bet_main_article: { populate: "*" },
+      },
+    },
+    true,
+  );
 
-  console.log("Homepage", homepage)
+  console.log("Homepage", homepage);
 
   return (
-    <div className="flex flex-col w-full items-center py-16 gap-16 min-h-screen ">
-      
+    <main className="m-auto flex min-h-screen w-full max-w-[1320px] flex-col items-center gap-8 py-10 sm:items-start">
+      <Image src={strapiImage(homepage.top_banner.image.url)} alt={""} width={1320} height={48} />
 
-      <main className="flex flex-col gap-8  items-center sm:items-start">
-        <Image src={strapiImage(homepage.top_banner.image.url)} alt={""} width={1320} height={48} />
-
-        {/*First row */}
-        <div className="flex gap-6">
-          <FeaturedArticleCard
-            id={homepage.main_article.id}
-            title={homepage.main_article.title}
-            description={homepage.main_article.description}
-            slug={homepage.main_article.slug}
-            image={homepage.main_article.image}
-            categories={homepage.main_article.categories}
-            content={homepage.main_article.content}
-            documentId={homepage.main_article.documentId}
-            createdAt={homepage.main_article.createdAt}
-            updatedAt={homepage.main_article.updatedAt}
-            publishedAt={homepage.main_article.publishedAt}
-            dynamic_zone={homepage.main_article.dynamic_zone}
-            seo={homepage.main_article.seo}
-          />
-        </div>
-        
-      </main>
-
-    </div>
+      {/*First row */}
+      <div className="flex w-full gap-6">
+        <FeaturedArticleCard
+          id={homepage.main_article.id}
+          title={homepage.main_article.title}
+          description={homepage.main_article.description}
+          slug={homepage.main_article.slug}
+          image={homepage.main_article.image}
+          categories={homepage.main_article.categories}
+          content={homepage.main_article.content}
+          documentId={homepage.main_article.documentId}
+          createdAt={homepage.main_article.createdAt}
+          updatedAt={homepage.main_article.updatedAt}
+          publishedAt={homepage.main_article.publishedAt}
+          dynamic_zone={homepage.main_article.dynamic_zone}
+          seo={homepage.main_article.seo}
+        />
+      </div>
+    </main>
   );
 }
