b99e344ed72ff0420cdb3f0d282631ebed167b2e
oarthurfc <arthuryouface@gmail.com>
2025-09-08 15:02:01 -0300

    feat: reorganizar middlewares para incluir configuração de segurança com políticas de conteúdo

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,9 +1,22 @@
 export default [
-  'strapi::logger',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'date', 'blob', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null
+        },
+      },
+    },
+  },
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
