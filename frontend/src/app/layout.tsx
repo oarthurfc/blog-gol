befd57ca1bf190c38559a82886342baf3ebcf7b5
oarthurfc <arthuryouface@gmail.com>
2025-09-16 12:04:49 -0300

    refactor: simplificar a função generateMetadata e substituir fetchContentType por getGlobalData no layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import fetchContentType from "@/lib/strapi/fetchContentType";
-import { generateMetadataObject } from "@/lib/metadata";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { getGlobalData } from "@/services/global";
 
 const interSans = Inter({
   variable: "--font-inter-sans",
@@ -12,44 +11,15 @@ const interSans = Inter({
 });
 
 // Default Global SEO for pages without them
-export async function generateMetadata({
-  params,
-}: {
-  params: { locale: string; slug: string };
-}): Promise<Metadata> {
-  const pageData = await fetchContentType(
-    "global",
-    {
-      filters: { locale: params.locale },
-      populate: "seo.metaImage",
-    },
-    true,
-  );
-
-  const seo = pageData?.seo;
-  const metadata = generateMetadataObject(seo);
-  return metadata;
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Blog GOL",
+    description: "Seu blog de futebol favorito",
+  };
 }
 
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const pageData = await fetchContentType(
-    "global",
-    {
-      populate: {
-        navbar: {
-          populate: ["logo", "right_navbar_items"],
-        },
-        footer: {
-          populate: ["logo", "internal_links", "policy_links"],
-        },
-      },
-    },
-    true,
-  );
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const pageData = await getGlobalData();
 
   console.log("Layout data:", pageData);
 
