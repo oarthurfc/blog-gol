478ecc6cdc1e0982dc4fb2ccdad3def9bf641ece
oarthurfc <arthuryouface@gmail.com>
2025-09-16 11:55:36 -0300

    fix: permitir que a função generateMetadataObject aceite dados nulos para melhorar a robustez

--- frontend/src/lib/metadata.ts
+++ frontend/src/lib/metadata.ts
@@ -1,19 +1,19 @@
 import { SEO } from "@/types/seo";
 
-export function generateMetadataObject(seo: SEO) {
+export function generateMetadataObject(seo?: SEO) {
   return {
-    title: seo.metaTitle || "Default Title",
-    description: seo.metaDescription || "Default Description",
+    title: seo?.metaTitle || "Default Title",
+    description: seo?.metaDescription || "Default Description",
     openGraph: {
-      title: seo.openGraph?.ogTitle || "Default OgTitle",
-      description: seo.openGraph?.ogDescription || "Default Description",
-      images: seo.openGraph?.ogImage ? [{ url: seo.openGraph.ogImage.url }] : [],
+      title: seo?.openGraph?.ogTitle || "Default OgTitle",
+      description: seo?.openGraph?.ogDescription || "Default Description",
+      images: seo?.openGraph?.ogImage ? [{ url: seo.openGraph.ogImage.url }] : [],
     },
     twitter: {
-      card: seo.openGraph?.ogImage || "summary_large_image",
-      title: seo.metaTitle || "Default Title",
-      description: seo.metaDescription || "Default Description",
-      images: seo.openGraph?.ogImage ? [{ url: seo.openGraph?.ogImage }] : [],
+      card: seo?.openGraph?.ogImage || "summary_large_image",
+      title: seo?.metaTitle || "Default Title",
+      description: seo?.metaDescription || "Default Description",
+      images: seo?.openGraph?.ogImage ? [{ url: seo.openGraph?.ogImage }] : [],
     },
   };
 }
