2a39ae267f28ab2910894f038d1fbd773e580a6d
oarthurfc <arthuryouface@gmail.com>
2025-09-24 12:06:00 -0300

    feat: add article data fetching to Home component for enhanced content display

--- frontend/src/app/(site)/teste/top-banner/page.tsx
+++ frontend/src/app/(site)/teste/top-banner/page.tsx
@@ -1,12 +1,15 @@
+import { getArticleBySlug } from "@/services/articles";
 import { getHomepageTopBanner } from "@/services/homepage-top-banner";
 
 export default async function Home() {
   const home = await getHomepageTopBanner();
+  const article = await getArticleBySlug("artigo-teste");
 
   return (
     <div className="m-auto flex min-h-screen w-full max-w-[1320px] flex-col items-center py-10 sm:items-start">
       <h1>Página de Teste Top Banner</h1>
       <pre>{JSON.stringify(home, null, 2)}</pre>
+      <pre>{JSON.stringify(article, null, 2)}</pre>
     </div>
   );
 }
