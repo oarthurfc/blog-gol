860795ff52d8c18f1600f6fa37c7591f14ca4892
oarthurfc <arthuryouface@gmail.com>
2025-10-07 23:26:51 -0300

    fix: update Article interface to use Author type for author details

--- frontend/src/types/article.ts
+++ frontend/src/types/article.ts
@@ -2,6 +2,7 @@ import { StrapiImage, StrapiData } from "@/types";
 import { Category } from "./category";
 import { SEO } from "./seo";
 import { BlocksContent } from "@strapi/blocks-react-renderer";
+import { Author } from "./author";
 
 export interface Article {
   id: number;
@@ -14,9 +15,7 @@ export interface Article {
   categories?: Category[];
   publishedAt: string;
   seo?: SEO;
-  author?: {
-    name: string;
-  };
+  author?: Author;
   relatedArticles?: {
     articles: {
       data: StrapiData<Article>[];
---
+++ frontend/src/types/author.ts
@@ -0,0 +1,6 @@
+export interface Author {
+  first_name: string;
+  last_name: string;
+  username?: string;
+  gender?: "male" | "female";
+}
