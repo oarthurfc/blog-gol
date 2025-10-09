3e538c0d44a3ed09fb1a8b18ee80ce3083171aeb
oarthurfc <arthuryouface@gmail.com>
2025-10-09 19:53:04 -0300

    feat: adjust heading class order for consistency and update Mais lidos section to use Link component

--- frontend/src/app/(site)/page.tsx
+++ frontend/src/app/(site)/page.tsx
@@ -6,6 +6,7 @@ import { generateMetadataObject } from "@/lib/metadata";
 import { getArticles } from "@/services/articles";
 import { getHomepage } from "@/services/homepage";
 import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 5;
 
@@ -79,7 +80,7 @@ export default async function Home() {
 
       {/*Apostas */}
       <div className="flex w-full flex-col gap-5">
-        <h2 className="text-heading text-3xl font-bold">Apostas</h2>
+        <h2 className="text-3xl font-bold text-heading">Apostas</h2>
         <div className="max-h-[678px]">
           {betArticle && <FeaturedArticleCard flex="row" {...betArticle} />}
         </div>
@@ -92,16 +93,17 @@ export default async function Home() {
 
       {/* Mais lidos */}
       <div className="flex w-full flex-col gap-5 py-14">
-        <h2 className="text-heading text-3xl font-bold">Mais lidos</h2>
+        <h2 className="text-3xl font-bold text-heading">Mais lidos</h2>
         <div className="grid grid-cols-2 grid-rows-4 gap-x-4 gap-y-2">
           {articles.slice(0, 8).map((article, index) => (
-            <div
+            <Link
               key={article.id || article.slug}
+              href={`/articles/${article.slug}`}
               className="border- flex flex-row items-center gap-6 rounded-md border border-background bg-card px-3 py-2"
             >
               <span className="text-4xl font-bold text-primary-yellow">{index + 1}</span>
               <p className="font-bold">{article.title}</p>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
