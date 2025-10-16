66e054546077c789a027c0dce7d72626f3ce4fcd
oarthurfc <arthuryouface@gmail.com>
2025-10-15 22:09:45 -0300

    refactor: enhance layout and structure of UltimasNoticias component for better readability and image display

--- frontend/src/components/UltimasNoticias.tsx
+++ frontend/src/components/UltimasNoticias.tsx
@@ -2,22 +2,43 @@ import { formatDateShort } from "@/lib/helpers";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { getArticles } from "@/services/articles";
+import Image from "next/image";
+import cloudinaryLoader from "@/lib/cloudinary";
 
 export default async function UltimasNoticias() {
   const articles = await getArticles(1, 15);
+
   return (
     <div className="flex h-full w-full flex-col justify-between gap-4 rounded-sm bg-card px-4 py-5 lg:max-h-[768px]">
-      <div className="flex max-h-[95%] flex-col gap-2">
+      <div className="flex max-h-[90%] flex-col gap-2">
         <h3 className="text-xl font-bold text-heading lg:text-2xl">Últimas notícias</h3>
         <ul className="overflow-y-auto pb-2">
           {articles.slice(3, 15).map((article) => (
             <Link
               href={`/artigos/${article.slug}`}
               key={article.id || article.slug}
-              className="flex flex-col border-b py-3 font-bold"
+              className="flex flex-row items-center gap-3 border-b py-3 font-bold"
             >
-              <span className="text-xs text-heading">{formatDateShort(article.publishedAt)}</span>
-              <span className="text-sm lg:text-base">{article.title}</span>
+              {/* Thumbnail da imagem */}
+              <div className="relative h-14 w-20 flex-shrink-0 overflow-hidden rounded">
+                {article.image?.url ? (
+                  <Image
+                    loader={cloudinaryLoader}
+                    src={article.image.url}
+                    alt={article.image.alternativeText || article.title}
+                    fill
+                    className="object-cover"
+                  />
+                ) : (
+                  <div className="h-full w-full bg-gray-200 dark:bg-gray-700"></div>
+                )}
+              </div>
+
+              {/* Conteúdo do texto */}
+              <div className="flex flex-1 flex-col">
+                <span className="text-xs text-heading">{formatDateShort(article.publishedAt)}</span>
+                <span className="line-clamp-2 text-xs lg:text-sm">{article.title}</span>
+              </div>
             </Link>
           ))}
         </ul>
